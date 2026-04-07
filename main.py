0a863a3b38e74665628821fee7d120b8c33a716c
Spoorthi <your-email@example.com>
2026-04-07 21:47:22 +0530

    Added class analytics, grading system, and pass/fail logic

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
-
-=======
 student_data = []
 while True:
     student_name = input("Enter Student Name: ")
-    student_marks = input("Enter Student Marks: \n").split()
+    student_marks = input("Enter Student Marks: ").split()
     marks_split = list(map(int, student_marks))
     student_data.append({"name": student_name, "marks":marks_split})
 
@@ -18,26 +15,65 @@ while True:
 
 topper = None
 high_avg = float('-inf')
+student_count = 0
+class_avg = 0
+pass_count = 0
+fail_count = 0
 
 for student in student_data:
+    student_count += 1
+
     marks_join = ", ".join(map(str, student['marks']))
 
     avg = sum(student['marks']) / len(student['marks'])
 
+    class_avg += avg
+
+    if(avg >= 90):
+        grade = 'A'
+        feedback = 'Excellent'
+    elif(avg >= 75):
+        grade = 'B'
+        feedback = 'Good'
+    elif(avg >= 60):
+        grade = 'C'
+        feedback = 'Average'
+    elif(avg >= 50):
+        grade = 'D'
+        feedback = 'Needs Improvement'
+    else:
+        grade = 'Fail'
+        feedback = "Work Hard"
+
     status = 'PASS'
-    for mark in student["marks"]:
-        if(mark < 35):
+    for mark in student['marks']:
+        if mark < 35:
             status = 'FAIL'
             break
 
+    if status == 'FAIL':
+        fail_count += 1
+    else:
+        pass_count += 1
     if(avg > high_avg):
         high_avg = avg
         topper = student['name']
 
+
+    print("-"*20)
     print(f"Name: {student['name']}")
     print(f"Marks: {marks_join}")
     print(f"Average: {avg:.2f}%")
-    print(f"Status: {status}\n")
+    print(f"Grade: {grade}")
+    print(f"Feedback: {feedback}")
+    print(f"Status: {status}")
+    print("-"*20)
 
-print(f"Topper: {topper} with average {high_avg:.2f}")
->>>>>>> 88a72c7 (Added student performance analyzer project)
+print(f"\nTopper: {topper} with average {high_avg:.2f}\n")
+
+print("="*10,"Class Analysis","="*10)
+print(f"Total Students: {student_count}")
+print(f"Class Average: {class_avg/student_count:.2f}%")
+print(f"Passed: {pass_count}")
+print(f"Failed: {fail_count}")
+print("="*20)
